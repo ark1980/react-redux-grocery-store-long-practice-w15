1c81b8c7459d4e70f6eb7db4be1c49e9d2705ef9
Ali Keshanian <ar.k@outlook.com>
2023-10-25 11:52:33 -0700

    Implemented feature: Open cart sidebar when adding a new item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import { useState } from 'react';
-import Cart from './components/Cart';
-import ProduceList from './components/ProduceList';
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import Cart from "./components/Cart";
+import ProduceList from "./components/ProduceList";
 
 function App() {
   const [showCart, setShowCart] = useState(false);
-  
+
+  const cart = useSelector((state) => state.cart);
+  useEffect(() => {
+    if (Object.keys(cart).length !== 0) setShowCart(true);
+  }, [cart]);
+
   return (
     <>
       <nav>
@@ -14,12 +20,12 @@ function App() {
           Checkout
         </button>
       </nav>
-      <main style={showCart ? { marginRight: '300px' } : {}} >
+      <main style={showCart ? { marginRight: "300px" } : {}}>
         <ProduceList />
       </main>
       <div
         className="sidebar"
-        style={showCart ? { transform: 'translateX(-100%)' } : {}}
+        style={showCart ? { transform: "translateX(-100%)" } : {}}
       >
         <div className="sidebar-header">
           <button className="arrow-button" onClick={() => setShowCart(false)}>
